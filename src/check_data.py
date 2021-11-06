f03e0bd3a6651d05d744a5f9e254f39aea3e4db6
0525stone <0525stone@gmail.com>
2021-11-06 21:05:45 +0900

    update : file save

--- src/check_data.py
+++ src/check_data.py
@@ -43,55 +43,63 @@ def read_data(dir_path = os.getcwd()+'/data/segmentation/'):
     for idx, (image, mask, mask2) in enumerate(zip(image_list, mask_list, mask2_list)):
       # 일단 한장
 
-        if idx<30:
-            print(f'{idx+1} 번째 : {image}, {mask}')
+        # if idx<5:
+        print(f'{idx+1} 번째 : {image}, {mask}')
 
-            img = cv2.imread(img_path+image)  # 오른쪽 사진
-            msk = cv2.imread(mask_path+mask)  # 왼쪽 사진
-            msk2 = cv2.imread(mask2_path+mask2)
+        img = cv2.imread(img_path+image)  # 오른쪽 사진
+        msk = cv2.imread(mask_path+mask)  # 왼쪽 사진
+        msk2 = cv2.imread(mask2_path+mask2)
 
-            print(f'image shape : {img.shape}, mask shape : {msk.shape}')
-            # img_show = cv2.hconcat([img,msk])
-            # print(img_show.shape)
+        print(f'image shape : {img.shape}, mask shape : {msk.shape}')
+        # img_show = cv2.hconcat([img,msk])
+        # print(img_show.shape)
 
-            # mask 부분
-            _msk = msk[:,:,0]
-            _msk = cv2.cvtColor(_msk, cv2.COLOR_BGR2RGB)
-            # _msk = cv2.bitwise_not(_msk)
-            # _msk_ = np.zeros(_msk.shape)
+        # mask 부분
+        _msk = msk[:,:,0]
+        _msk = cv2.cvtColor(_msk, cv2.COLOR_BGR2RGB)
+        # _msk = cv2.bitwise_not(_msk)
+        # _msk_ = np.zeros(_msk.shape)
 
-            # mask2
-            _msk2 = msk2[:, :, 0]
-            _msk2 = cv2.cvtColor(_msk2, cv2.COLOR_BGR2RGB)
-            # _msk2 = cv2.bitwise_not(_msk2)
-            # _msk2_ = np.zeros(_msk2.shape)
+        # mask2
+        _msk2 = msk2[:, :, 0]
+        _msk2 = cv2.cvtColor(_msk2, cv2.COLOR_BGR2RGB)
+        # _msk2 = cv2.bitwise_not(_msk2)
+        # _msk2_ = np.zeros(_msk2.shape)
 
-            print('shape : ',_msk2.shape)
+        print('shape : ',_msk2.shape)
 
-            color_mask = cv2.applyColorMap(_msk, cv2.COLORMAP_OCEAN)
-            color_mask2 = cv2.applyColorMap(_msk2, cv2.COLORMAP_OCEAN)
-            print('color_mask2 : ',color_mask2.shape)
-            # color_mask = _msk
-            # color_mask2 = _msk2
-
-
-            # mask 색깔 단일로 만들어야 보기 좋음
-            color_mask[:, :, 0] = 0 #np.zeros(_msk.shape)
-            color_mask[:, :, 1] = 0 #np.zeros(_msk.shape)
-            # color_mask[:, :, 2] = np.zeros(_msk.shape)
-            # color_mask2[:, :, 0] = np.ones(_msk2.shape)
-            color_mask2[:, :, 1] = 0 #np.zeros(_msk2.shape)
-            color_mask2[:, :, 2] = 0#np.zeros(_msk2.shape)
+        color_mask = cv2.applyColorMap(_msk, cv2.COLORMAP_OCEAN)
+        color_mask2 = cv2.applyColorMap(_msk2, cv2.COLORMAP_OCEAN)
+        print('color_mask2 : ',color_mask2.shape)
+        # color_mask = _msk
+        # color_mask2 = _msk2
 
 
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            img_show = cv2.addWeighted(img, 1, color_mask, 0.2, 0.0)
-            img_show = cv2.addWeighted(img_show, 1, color_mask2, 0.4, 0.0)
+        # mask 색깔 단일로 만들어야 보기 좋음
+        color_mask[:, :, 0] = 0 #np.zeros(_msk.shape)
+        color_mask[:, :, 1] = 0 #np.zeros(_msk.shape)
+        # color_mask[:, :, 2] = np.zeros(_msk.shape)
+        # color_mask2[:, :, 0] = np.ones(_msk2.shape)
+        color_mask2[:, :, 1] = 0 #np.zeros(_msk2.shape)
+        color_mask2[:, :, 2] = 0#np.zeros(_msk2.shape)
 
 
+        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # img_show = img
+        img_show = cv2.addWeighted(img, 1, color_mask, 0.2, 0.0)
+        img_show = cv2.addWeighted(img_show, 1, color_mask2, 0.4, 0.0)
+
+      # save file part
+        save_path = './data/answer1/'
+        save_file = save_path+image
+        print(save_file)
+        cv2.imwrite(save_file, img_show)
+
+
+        #
+        _show = 0
+        if(_show):
             plt.imshow(img_show)
-            # plt.imshow(_msk2)
-            # print(_msk2)
             plt.waitforbuttonpress(-1)
             plt.close()
 
